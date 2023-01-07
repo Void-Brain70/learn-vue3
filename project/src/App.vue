6d2e87723d8d89e3ab451a54305f0ed26d21942f
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2023-01-07 17:23:10 +0600

    interpolation

---
+++ project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <HelloWorld msg="Welcome to Vue3" />
+    <HomePage></HomePage>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import HomePage from "./components/HomePage.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    HomePage,
+  },
+};
+</script>
+
+<style></style>
---
+++ project/src/components/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Information</h1>
+        <h3> Name -> {{ name }}</h3>
+        <h3> ID -> {{ id }}</h3>
+        <h3> ID -> {{ id = '18101071'}}</h3>
+        <h3> Department -> {{ dept }}</h3>
+    </div>
+</template>
+<script>
+export default {
+    name : "HomePage",
+    data(){
+        return {
+            name : "Anik Das",
+            id : "18101070",
+            dept : "CSE"
+        }
+    }
+}
+</script>
+<style scoped>
+h1{
+    color: red;
+}
+</style>
