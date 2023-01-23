478dff9ce634494c1c639555af489e0ddfd04e37
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2023-01-23 10:38:53 +0600

    html binding class binging

--- project/src/components/HelloWorld.vue
+++ project/src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div v-html="tag">
+    </div>
+    <h3 :class="{color:tagColor}">Class Binding</h3>
+    <button v-on:click="tagColor=!tagColor">Apply Color</button>
   </div>
 </template>
 
@@ -9,11 +13,19 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      tag : '<h3>HTML Binding</h3>',
+      tagColor : false
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.color{
+  background-color: cornflowerblue;
+}
 </style>
