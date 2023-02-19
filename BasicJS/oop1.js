0d5c8fbf5ed67a59ad8ad39bbf66d2059ea2000f
Anik Chandra <67729889+Void-Brain70@users.noreply.github.com>
2023-02-19 12:54:02 +0600

    oop

--- BasicJS/oop1.js
+++ BasicJS/oop1.js
@@ -12,6 +12,17 @@ function personInfo(name,phone,date_of_birth){
 const info = new personInfo("Anik Das",'01626838003', '1998-08-18');
 console.log(info.ageCalculate());
 
+
+function playerInfo(name,club,national_team,goal){
+    this.name = name;
+    this.club = club;
+    this.national_team = national_team;
+    this.goal = goal;
+}
+const palyer = new playerInfo('Messi','PSG','Argentina','779');
+console.log(palyer);
+
+
 class car {
     constructor(name,model,color){
         this.name=  name;
