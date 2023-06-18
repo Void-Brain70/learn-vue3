343ea25de44ff9e47a1f396e362ef95dbacfd7cd
Anik Chandra <dasssanik124102@gmail.com>
2023-06-18 22:34:38 +0600

    pinia start

--- portfolio/src/views/Contact.vue
+++ portfolio/src/views/Contact.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
         <h1>Contact Page</h1>
+        {{ counterStore.title }}
+
     </div>
 </template>
+
+<script setup>
+import { useCounterStore } from '../store/counter';
+
+const counterStore = useCounterStore();
+
+</script>
