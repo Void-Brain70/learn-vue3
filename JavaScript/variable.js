04a336c948399ae828a48643b24e31996a81c633
Anik Chandra <dasssanik124102@gmail.com>
2023-07-06 15:21:02 +0600

    start

---
+++ JavaScript/variable.js
@@ -0,0 +1,22 @@
+// Variable type
+// String Numeric Boolean 
+
+var price = 101;
+
+var name = 'Anik Das'
+
+var bol = true
+
+//Mathematical operation 
+
+var pant = 1200;
+var shirt = 1100;
+var quantity = 3;
+var total = pant + shirt;
+var priceDiff = pant - shirt;
+var totalMulti = pant * quantity; 
+
+// document.write(price, name, bol, total);
+
+
+
