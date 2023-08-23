615e7727132134d58f8c50177d9dea4df323897f
Anik Chandra <dasssanik124102@gmail.com>
2023-08-23 16:57:09 +0600

    sdfsdf

---
+++ JavaScript/dayOne.js
@@ -0,0 +1,70 @@
+//JavaScript Output
+// alert('Anik Das');
+document.write('Anik Das');
+console.log(typeof('Anik Das'));
+
+//JavaScript Keyword, Data Type, Comment
+
+//JavaScript Variable(let, var, const)
+let name = 'Anik Das';
+name = 'Koyel';
+document.write(name);
+
+//old version
+// var a = 5;
+// var b = 4;
+// var result = a + b;
+// document.write(result);
+
+let x = 5;
+let y = 4;
+let sum = x + y;
+console.log(sum);
+
+//JavaScript Number Method
+let num = 70;
+num = toString(num);
+console.log(typeof (num));
+
+let i = '70';
+let f = '3.74'
+
+ri = parseInt(i);
+rf = parseFloat(f);
+
+console.log(typeof (ri));
+console.log(typeof (rf));
+
+let number = 3.1416;
+console.log(number.toFixed(2));
+console.log(number.toPrecision(2));
+
+//String Concatenate
+let firstName = 'Anik'
+let lastName = " Chandra"
+let sc = firstName + lastName;
+console.log(sc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- JavaScript/stringMethod.js
+++ JavaScript/stringMethod.js
@@ -1,8 +1,14 @@
 // String length 
-let text = "Anik Das";
+let text = "JavaScript String Methods";
+let a = 'Anik'
+let k = 'Koyel'
 let length = text.length;
-document.write(length);
-
+console.log(length);
+console.log(text.charAt(2));
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(a.concat(k));
+console.log(k.slice(1,4));
 //String slice(start, end)
 let slice = text.slice(2,4);
 document.write(slice);
