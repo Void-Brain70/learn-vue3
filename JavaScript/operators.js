5af659b37c4480e30108ef9d74a892b1d88f8d52
Anik Chandra <dasssanik124102@gmail.com>
2023-08-26 11:23:21 +0600

    operators

---
+++ JavaScript/operators.js
@@ -0,0 +1,22 @@
+console.log('Operators')
+
+//JavaScript Arithmetic Operators
+let a = 20;
+let b = 5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a%b);
+console.log(a**b);
+console.log(a++);
+console.log(a);
+console.log(a--);
+console.log(a);
+console.log(++b);
+console.log(--b);
+
+console.log((5>3)? 'ok': 'notOk');
+
+
