196367dcbf07906441c529df499ba265f6c9b25a
Anik Chandra <dasssanik124102@gmail.com>
2023-09-01 18:05:29 +0600

    function object

--- JavaScript/stringMethod.js
+++ JavaScript/stringMethod.js
@@ -3,12 +3,25 @@ let text = "JavaScript String Methods";
 let a = 'Anik'
 let k = 'Koyel'
 let length = text.length;
-console.log(length);
-console.log(text.charAt(2));
-console.log(text.toUpperCase());
-console.log(text.toLowerCase());
-console.log(a.concat(k));
-console.log(k.slice(1,4));
+// console.log(length);
+// console.log(text.charAt(2));
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+// console.log(a.concat(k));
+// console.log(k.slice(1,4));
+
 //String slice(start, end)
-let slice = text.slice(2,4);
-document.write(slice);
+let data = "Apple, Banana, Kiwi";
+// console.log(data.slice(7));
+// console.log(data.slice(-12));
+// console.log(data.slice(-12,-6));
+
+//Replace
+// let str = "koyel and koyel";
+// console.log(str.replace('koyel', 'Aporna'));
+
+let str = 'I love Sokina';
+console.log(str.substr(7, 12));
+
+let p = '7';
+console.log(p.padStart(4,7));
---
+++ JavaScript/templateLiterals.js
@@ -0,0 +1,3 @@
+let str = "Koyel";
+let result = `I love ${str}`;
+console.log(result);
